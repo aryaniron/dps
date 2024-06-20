068df952b309c3b8788a9555936e663528da69b3
dev-catalyst <riya.barua@catalyst.sh>
2024-06-21 02:52:30 +0530

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,17 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+});
+
+app.enableCors({
+    allowedHeaders:"*",
+    origin: "*"
+});
   // Disable frameguard to remove X-Frame-Options header
   app.use(
     helmet({
